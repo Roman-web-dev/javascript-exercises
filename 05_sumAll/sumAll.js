4a9020fa709e2c9c6ffaae3cd7d6830623000b87
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-09-23 15:42:03 -0400

    Coomplete Task 5 using Gauss formula

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+
+    if(firstNum < 0 || secondNum < 0 ||
+        !Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR"
+    }
+    
+    let largest, smallest
+
+    if (firstNum < secondNum) {
+         largest = secondNum
+         smallest = firstNum
+    } else {
+         largest = firstNum
+         smallest = secondNum
+    }
+
+    let n = largest - smallest + 1;
+    let sum = (n * (smallest + largest)) / 2;
+    
+    return sum
 
 };
 
