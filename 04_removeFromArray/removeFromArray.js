3d54ecd2798523483e41a1cde53f8fae7a4dfaf5
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-09-23 14:52:45 -0400

    Complete Task 4 with using while loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+
+    let arr = arguments[0]
+    for (let i = 1; i < arguments.length; i++) {
+        while (arr.includes(arguments[i])) {
+           
+            arr.splice(arr.indexOf(arguments[i]), 1)
+           
+        } 
+    }
+    console.log(arr)
+    return arr
+
 };
 
 // Do not edit below this line
