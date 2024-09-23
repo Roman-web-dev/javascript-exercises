74ce4b894718eb8dd817f73598e40b2908c326d7
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-09-23 12:32:11 -0400

    Create a conditions and loop to pass all tests
    Conditions are created with usind ternary operators

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    let output
+   
+    num >= 1 ? output = text : 
+    num < 0 ? output = "ERROR" : output = "";
+
+    for (i = 1; i < num; i++) {
+        output += text
+    }
+    
+    return output
 
 };
 
