61af6a1831cd789dc8e50535ad1177b2c73cf726
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-09-23 13:13:25 -0400

    Complete Task 3 with using for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    
+    let reversed =[]
 
+    for (i = text.length; i >= 0; i--) {
+        reversed.push(text[i])
+    }
+
+    reversed.shift()
+
+    return reversed.join("")
 };
 
 // Do not edit below this line
