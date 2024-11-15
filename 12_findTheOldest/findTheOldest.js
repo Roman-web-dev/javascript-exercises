b6b6c3c287cadb0991068f69904d8a834486491b
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-11-15 15:43:52 -0500

    Complete tasks 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const currentYear = new Date()
+    return people.reduce((oldest, item) => {
+        // Check oldest is empty
+        if(!oldest.name) {
+            return item
+            
+        }
+        
+        // Calculate age of oldest
+        let oldestAge 
+        if (oldest.yearOfDeath === undefined) {
+            oldestAge =  currentYear.getFullYear() - oldest.yearOfBirth
+        } else {
+            oldestAge =  oldest.yearOfDeath - oldest.yearOfBirth
+        }
+        
+        // Calculate age of person
+        let age  
+        if (item.yearOfDeath === undefined) {
+            age =  currentYear.getFullYear() - item.yearOfBirth
+        } else {
+            age = item.yearOfDeath - item.yearOfBirth
+        }
+        
+        // Find new oldest person
+        if (age > oldestAge) {
+            return item
+        } else return oldest
+        
+    }, {})
+}
+;
 
 // Do not edit below this line
 module.exports = findTheOldest;
